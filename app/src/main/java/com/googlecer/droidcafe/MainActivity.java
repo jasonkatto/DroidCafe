fd47ecde780318614cb9e6cdc9cb041fe3c4dd09
JAson <wayousif2@oakland.edu>
2020-01-21 19:45:06 -0500

    DroidCafe

---
+++ app/src/main/java/com/googlecer/droidcafe/MainActivity.java
@@ -0,0 +1,108 @@
+package com.googlecer.droidcafe;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Tag for the intent extra.
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.droidcafe.extra.MESSAGE";
+    ImageView donut,iceCream,froyo;
+
+    // The order message, displayed in the Toast and sent to the new Activity.
+    private String mOrderMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        donut= findViewById(R.id.donut);
+        iceCream=findViewById(R.id.ice_cream);
+        froyo=findViewById(R.id.froyo);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order));
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact));
+                return true;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Shows a message that the donut image was clicked.
+     */
+    public void  showDonutOrder(View view){
+        displayToast(mOrderMessage);
+     }
+
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Shows a message that the ice cream sandwich image was clicked.
+     */
+    public void showFroyoOrder(View view){
+        displayToast(mOrderMessage);
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked.
+     */
+    public void showIceCreamOrder(View view){
+        displayToast(mOrderMessage);
+    }
+}
